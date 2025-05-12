be767940a15b1eee5df6a46991c9eeae7c0e14cc
Mariana U <mariana.urrego@upb.edu.co>
2025-05-12 07:17:57 -0500

    fix(paginas): :sparkles: correción crear oferta y se añadió la interfaz para agregar pdfs
    se agregó un file input multiple para que recibiera varios tipos de archivos, revisar los otros componentes por si otro sirve, agregué el campo de perfil como un área de texto, queda pendiente lo del resumen de la oferta, tipo habilidades blandas, tecn. etc. recibe el item del dropdown, pero toca que sean acumulativos

---
+++ hireflow_dev/src/components/forms/form-elements/fileinput/FileInput.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-file-input
+      multiple
+      label="Subir archivos pdfs"
+      hide-details
+      variant="outlined"
+    ></v-file-input>
+  </template>
--- hireflow_dev/src/views/dashboards/createOffer/index.vue
+++ hireflow_dev/src/views/dashboards/createOffer/index.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 /* Breadcrumb component */
 import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
 
 import UiParentCard from '@/components/shared/UiParentCard.vue';
 
+import UiChildCard from '@/components/shared/UiChildCard.vue';
 /*Call Components*/
 import CardsDashboard from '@/components/dashboards/dashboard2/CardsDashboard.vue';
 import TableOffer from '@/components/table/TableOffer.vue';
-import RequerimentsCard from '@/components/dashboards/dashboard2/RequerimentsCard.vue';
-import RequerimentsCard2 from '@/components/dashboards/dashboard2/RequirementsCardsText.vue';
+
+/*inputs*/
+const nameOffer = ref('input para el nombre de la oferta');
 // template breadcrumb
-const page = ref({ title: 'Crear oferta' });
+const page = ref({ title: '' });
 const breadcrumbs = ref([
     {
         text: 'Home',
@@ -24,9 +26,94 @@ const breadcrumbs = ref([
         href: '#'
     }
 ]);
+import { useNoteStore } from '@/stores/apps/notes';
+import { colorVariation } from '@/_mockApis/apps/notes/index';
 
+const store = useNoteStore();
+const getNote = computed(() => {
+    return store.notes[store.notesContent - 1];
+});
+const locations = ref(['Finanzas', 'Marketing', 'RRHH', 'Desarrollo']);
+const location = ref();
+
+const hab_blandas = ref(['Comunicación', 'Trabajo en equipo']);
+const hab_blanda = ref();
+
+
+const hab_tecnicas = ref(['Java', 'SQL']);
+const hab_tecnica = ref();
+
+
+const estudios = ref(['Pre grado', 'Especialización']);
+const estudio = ref();
 </script>
 <template>
     <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
-    
+    <v-row>
+        <v-col cols="12" xl="5" class="py-3 pl-6 pr-4 d-flex align-center mb-5">
+                <v-row>
+                    <h3 class="display-1 textPrimary font-weight-bold ">Crear oferta</h3>
+                </v-row>
+        </v-col>
+        <v-col cols="12">
+                <v-card  variant="outlined">
+                    <v-card-item>
+                        <div class="mt-5">
+                            <v-row>
+                                <v-col cols="12" md="6">
+                                     <v-label class="mb-2 font-weight-medium">Nombre oferta</v-label>
+                                        <v-text-field
+                                            color="primary"
+                                            variant="outlined"
+                                            type="text"
+                                            v-model="nameOffer"
+                                            hide-details
+                                        />
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="12" md="6">
+                                    <div class="mt-4">
+                                        <v-label class="mb-2 font-weight-medium">Nombre departamento</v-label>
+                                        <v-select v-model="location" :items="locations" label="Seleccionar departamento"></v-select>
+                                    </div>
+                                </v-col>
+                            </v-row>
+                            <v-col>
+                                    <v-sheet>
+                                        <v-label class="mb-2 font-weight-medium">Perfil</v-label>
+                                        <v-textarea outlined name="Note" v-model="title"></v-textarea>
+                                    </v-sheet>
+                            </v-col>
+                        </div>
+                    </v-card-item>
+                </v-card>
+            </v-col>
+        
+        <UiParentCard title="Descripción resumida de la oferta">
+            <v-row>
+                <v-col cols="12" sm="12" lg="6">
+                    <UiChildCard title="Estudios necesarios">
+                        <v-select v-model="estudio" :items="estudios" label="Seleccionar área"></v-select>
+                    </UiChildCard>
+                </v-col>
+                <v-col cols="12" sm="12" lg="6">
+                    <UiChildCard title="Habilidades blandas">
+                        <v-select v-model="hab_blanda" :items="hab_blandas" label="Seleccionar área"></v-select>
+                    </UiChildCard>
+                </v-col>
+                <v-col cols="12" sm="12" lg="6">
+                    <UiChildCard title="Habilidades técnicas">
+                        <v-select v-model="hab_tecnica" :items="hab_tecnicas" label="Seleccionar área"></v-select>
+                    </UiChildCard>
+                </v-col>
+                <v-col cols="12" sm="12" lg="6">
+                    <UiChildCard title="Experiencia necesaria">
+                        <v-select v-model="location" :items="locations" label="Seleccionar área"></v-select>
+                    </UiChildCard>
+                </v-col>
+            </v-row>    
+        </UiParentCard>
+        
+    </v-row>
 </template>
