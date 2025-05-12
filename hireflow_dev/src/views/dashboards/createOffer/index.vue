b555604adf58da9e790d7763957c9dfbbcfb1197
Darch02 <juanajaramillom@hotmail.com>
2025-05-12 10:28:03 -0500

    feat: se ajustaron los campos del formuario de crear oferta, y se puede añadir un nuevo departamento

--- hireflow_dev/src/views/dashboards/createOffer/index.vue
+++ hireflow_dev/src/views/dashboards/createOffer/index.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 /* Breadcrumb component */
 import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
-
 import UiParentCard from '@/components/shared/UiParentCard.vue';
-
 import UiChildCard from '@/components/shared/UiChildCard.vue';
-/*Call Components*/
-import CardsDashboard from '@/components/dashboards/dashboard2/CardsDashboard.vue';
-import TableOffer from '@/components/table/TableOffer.vue';
+import { useNoteStore } from '@/stores/apps/notes';
+import CreateDepDialog from '@/components/dashboards/createDialog/createDepDialog.vue';
 
-/*inputs*/
-const nameOffer = ref('input para el nombre de la oferta');
 // template breadcrumb
-const page = ref({ title: '' });
+const page = ref({ title: 'crear oferta' });
 const breadcrumbs = ref([
     {
         text: 'Home',
@@ -26,26 +21,94 @@ const breadcrumbs = ref([
         href: '#'
     }
 ]);
-import { useNoteStore } from '@/stores/apps/notes';
-import { colorVariation } from '@/_mockApis/apps/notes/index';
 
 const store = useNoteStore();
 const getNote = computed(() => {
     return store.notes[store.notesContent - 1];
 });
-const locations = ref(['Finanzas', 'Marketing', 'RRHH', 'Desarrollo']);
-const location = ref();
 
-const hab_blandas = ref(['Comunicación', 'Trabajo en equipo']);
-const hab_blanda = ref();
+/*inputs*/
+const nameOffer = ref('input para el nombre de la oferta');
+
+const departamentos = ref<Array<{ id: number; nombre: string }>>([]);
+const depaSeleccionado = ref<string | null>(null);
+
+const hab_blandas = ref<string[]>([]);
+const nuevaHab_blanda = ref('');
 
 
-const hab_tecnicas = ref(['Java', 'SQL']);
-const hab_tecnica = ref();
+const hab_tecnicas = ref<string[]>([]);
+const nuevaHab_tecnica = ref("");
+
+const nuevoEstudio = ref('');
+const estudios = ref<string[]>([]);
+
+const experiencia = ref<string[]>([]);
+const nuevaExp = ref("")
+
+const agregarEstudio = () => {
+  const item = nuevoEstudio.value.trim();
+  if (item !== '') {
+    estudios.value.push(item);
+    nuevoEstudio.value = '';
+  }
+};
+
+const eliminarEstudio = (index: number) => {
+  estudios.value.splice(index, 1);
+};
+
+const agregarHabBlandas = () => {
+  const item = nuevaHab_blanda.value.trim();
+  if (item !== '') {
+    hab_blandas.value.push(item);
+    nuevaHab_blanda.value = '';
+  }
+};
+
+const eliminarHabBlanda = (index: number) => {
+  hab_blandas.value.splice(index, 1);
+};
+
+const agregarHabTecnicas = () => {
+  const item = nuevaHab_tecnica.value.trim();
+  if (item !== '') {
+    hab_tecnicas.value.push(item);
+    nuevaHab_tecnica.value = '';
+  }
+};
+
+const eliminarHabTecnica = (index: number) => {
+  hab_tecnicas.value.splice(index, 1);
+};
+
+const agregarExperiencia = () => {
+    const item = nuevaExp.value.trim();
+  if (item !== '') {
+    experiencia.value.push(item);
+    nuevaExp.value = '';
+  }
+}
+
+const eliminarExperiencia = (index: number) => {
+  experiencia.value.splice(index, 1);
+};
 
 
-const estudios = ref(['Pre grado', 'Especialización']);
-const estudio = ref();
+const perfil = ref()
+
+onMounted(async () => {
+  try {
+     // Obtener los departamentos
+    const resDepartamentos = await fetch('http://127.0.0.1:8000/ofertas/departamentos/')
+    if (!resDepartamentos.ok) throw new Error('Error al cargar departamentos')
+    const dataDepartamentos = await resDepartamentos.json()
+    departamentos.value = dataDepartamentos
+
+  } catch (error) {
+    console.error('Error al obtener los datos:', error)
+  }
+})
 </script>
 <template>
     <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
@@ -75,14 +138,21 @@ const estudio = ref();
                                 <v-col cols="12" md="6">
                                     <div class="mt-4">
                                         <v-label class="mb-2 font-weight-medium">Nombre departamento</v-label>
-                                        <v-select v-model="location" :items="locations" label="Seleccionar departamento"></v-select>
+                                        <v-row>
+                                            <v-col cols="12" md="6">
+                                                <v-select v-model="depaSeleccionado" :items="departamentos" item-title="nombre" item-value="nombre" label="Seleccionar departamento"></v-select>
+                                            </v-col>
+                                            <v-col cols="12" md="auto" class="text-right">
+                                                <CreateDepDialog />
+                                            </v-col>
+                                        </v-row>
                                     </div>
                                 </v-col>
                             </v-row>
                             <v-col>
                                     <v-sheet>
                                         <v-label class="mb-2 font-weight-medium">Perfil</v-label>
-                                        <v-textarea outlined name="Note" v-model="title"></v-textarea>
+                                        <v-textarea outlined name="Note" v-model="perfil"></v-textarea>
                                     </v-sheet>
                             </v-col>
                         </div>
@@ -94,22 +164,114 @@ const estudio = ref();
             <v-row>
                 <v-col cols="12" sm="12" lg="6">
                     <UiChildCard title="Estudios necesarios">
-                        <v-select v-model="estudio" :items="estudios" label="Seleccionar área"></v-select>
+                        <v-text-field
+                        label="Agregar item"
+                        v-model="nuevoEstudio"
+                        @keyup.enter="agregarEstudio()"
+                        append-inner-icon="mdi-plus"
+                        @click:append-inner="agregarEstudio()"
+                        ></v-text-field>
+                        <v-list v-if="estudios.length">
+                            <v-list-item
+                                v-for="(item, index) in estudios"
+                                :key="index"
+                            >
+                                <v-row>
+                                    <v-col>
+                                        <v-list-item-title>{{ item }}</v-list-item-title>
+                                    </v-col>
+                                    <v-col>
+                                        <v-btn icon color="error" density="compact" @click="eliminarEstudio(index)">
+                                            <v-icon>mdi-delete</v-icon>
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-list-item>
+                        </v-list>
                     </UiChildCard>
                 </v-col>
                 <v-col cols="12" sm="12" lg="6">
                     <UiChildCard title="Habilidades blandas">
-                        <v-select v-model="hab_blanda" :items="hab_blandas" label="Seleccionar área"></v-select>
+                        <v-text-field
+                        label="Agregar item"
+                        v-model="nuevaHab_blanda"
+                        @keyup.enter="agregarHabBlandas()"
+                        append-inner-icon="mdi-plus"
+                        @click:append-inner="agregarHabBlandas()"
+                        ></v-text-field>
+                        <v-list v-if="hab_blandas.length">
+                            <v-list-item
+                                v-for="(item, index) in hab_blandas"
+                                :key="index"
+                            >
+                                <v-row>
+                                    <v-col>
+                                        <v-list-item-title>{{ item }}</v-list-item-title>
+                                    </v-col>
+                                    <v-col>
+                                        <v-btn icon color="error" density="compact" @click="eliminarHabBlanda(index)">
+                                            <v-icon>mdi-delete</v-icon>
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-list-item>
+                        </v-list>
                     </UiChildCard>
                 </v-col>
                 <v-col cols="12" sm="12" lg="6">
                     <UiChildCard title="Habilidades técnicas">
-                        <v-select v-model="hab_tecnica" :items="hab_tecnicas" label="Seleccionar área"></v-select>
+                        <v-text-field
+                        label="Agregar item"
+                        v-model="nuevaHab_tecnica"
+                        @keyup.enter="agregarHabTecnicas()"
+                        append-inner-icon="mdi-plus"
+                        @click:append-inner="agregarHabTecnicas()"
+                        ></v-text-field>
+                        <v-list v-if="hab_tecnicas.length">
+                            <v-list-item
+                                v-for="(item, index) in hab_tecnicas"
+                                :key="index"
+                            >
+                                <v-row>
+                                    <v-col>
+                                        <v-list-item-title>{{ item }}</v-list-item-title>
+                                    </v-col>
+                                    <v-col>
+                                        <v-btn icon color="error" density="compact" @click="eliminarHabTecnica(index)">
+                                            <v-icon>mdi-delete</v-icon>
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-list-item>
+                        </v-list>
                     </UiChildCard>
                 </v-col>
                 <v-col cols="12" sm="12" lg="6">
                     <UiChildCard title="Experiencia necesaria">
-                        <v-select v-model="location" :items="locations" label="Seleccionar área"></v-select>
+                        <<v-text-field
+                        label="Agregar item"
+                        v-model="nuevaExp"
+                        @keyup.enter="agregarExperiencia()"
+                        append-inner-icon="mdi-plus"
+                        @click:append-inner="agregarExperiencia()"
+                        ></v-text-field>
+                        <v-list v-if="experiencia.length">
+                            <v-list-item
+                                v-for="(item, index) in experiencia"
+                                :key="index"
+                            >
+                                <v-row>
+                                    <v-col>
+                                        <v-list-item-title>{{ item }}</v-list-item-title>
+                                    </v-col>
+                                    <v-col>
+                                        <v-btn icon color="error" density="compact" @click="eliminarExperiencia(index)">
+                                            <v-icon>mdi-delete</v-icon>
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-list-item>
+                        </v-list>
                     </UiChildCard>
                 </v-col>
             </v-row>    
